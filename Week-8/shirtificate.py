c8aaf5cbc136dc01c25646ad1a7098dd914ca8d7
Shreejal Shah <shreejalshah21@gmail.com>
2026-07-03 01:21:39 +0545

    Add files via upload

---
+++ Week-8/shirtificate.py
@@ -0,0 +1,31 @@
+from fpdf import FPDF
+
+
+class PDF(FPDF):
+    def header(self):
+        self.image("shirtificate.png", 10, 70, 190)
+
+        # Title
+        self.set_font("helvetica", "", 48)
+        self.cell(0, 57, "CS50 Shirtificate", align="C")
+        self.ln(20)
+
+
+def main():
+    name = input("Name: ")
+    shirt(name)
+
+
+def shirt(name):
+    pdf = PDF()
+    pdf.add_page(orientation="portrait", format="a4")
+
+    pdf.set_font("helvetica", size=24)
+    pdf.set_text_color(255, 255, 255)
+    pdf.cell(0, 213, f"{name} took CS50", align="C")
+
+    pdf.output("shirtificate.pdf")
+
+
+if __name__ == "__main__":
+    main()
